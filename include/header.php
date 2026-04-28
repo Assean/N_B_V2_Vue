648a0c0173d9579007d7d7d98a1b535af5b9f969
sean <y20120816s@gmail.com>
2026-04-28 21:41:38 +0800

    v1.3(header)

--- include/header.php
+++ include/header.php
@@ -1,7 +1,12 @@
-<header>
-    <img src="" alt="">
-    <h3>Public Transit Query System 大眾運輸查詢系統</h3>
-    <a href="login.php">
-        
-    </a>
+<header class="shadow-sm p-3 d-flex w-100 align-items-center">
+    <!-- <h3 class="mb-0"></h3> -->
+    <a href="index.php" class="mb-0">Public Transit Query System 大眾運輸查詢系統</a>
+    <div class="ml-auto">    
+        <?php if(!isset($_SESSION['user'])){ ?>
+            <a href="./api/chack_login.php" class="ml-3">系統管理</a>
+        <?php }else{ ?>
+            <a href="./pages/admin_index.php" class="ml-3">系統管理</a>
+            <a href="./api/logout.php" class="ml-3">登出</a>
+        <?php } ?>
+    </div>
 </header>
